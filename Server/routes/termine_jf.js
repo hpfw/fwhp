c23cb47013480c8ee6bf07a47db07665fe509613
hpfw <fwhp@web.de>
2018-07-07 10:20:43 +0200

    update

--- Server/routes/termine_jf.js
+++ Server/routes/termine_jf.js
@@ -10,29 +10,37 @@ var user = require('../database/user')
 var wochentage = (param) => {
     var day;
 
-    switch(param) {
-        case 'Monday': day = 'Montag'
-                        break;
-        case 'Tuesday': day = 'Dienstag'
-                        break;
-        case 'Wednesday': day = 'Mittwoch'
-                        break;
-        case 'Thursday': day = 'Donnerstag'
-                        break;
-        case 'Friday': day = 'Freitag'
-                        break;  
-        case 'Saturday': day = 'Samstag'
-                        break;
-        case 'Sunday': day = 'Sonntag'
-                        break;
-        default: day = 'Mittwoch'
-                        break;
+    switch (param) {
+        case 'Monday':
+            day = 'Montag'
+            break;
+        case 'Tuesday':
+            day = 'Dienstag'
+            break;
+        case 'Wednesday':
+            day = 'Mittwoch'
+            break;
+        case 'Thursday':
+            day = 'Donnerstag'
+            break;
+        case 'Friday':
+            day = 'Freitag'
+            break;
+        case 'Saturday':
+            day = 'Samstag'
+            break;
+        case 'Sunday':
+            day = 'Sonntag'
+            break;
+        default:
+            day = 'Mittwoch'
+            break;
     }
 
     return day;
 }
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
     var con = mysql.createConnection({
         host: "localhost",
@@ -42,31 +50,56 @@ router.get('/', function(req, res) {
         database: "feuerweh_"
     });
 
-    con.connect(function(err) {
+    con.connect(function (err) {
         if (err) throw err;
 
         date.setDate(date.getDate() - 1);
-    
+
         con.query("SELECT * FROM termine_jf WHERE datum >= " + mysql.escape(date) + ' LIMIT 1', function (err, result, fields) {
-            //if (err) {
-            //    res.send({ datum: "02.04.2019", uhrzeit: "18:30", probe: "asdg", leiter: "125125", tag: "Montag" })
-                //throw err;
-            //}
-            //con.end();
+            if (err) {
+                res.send({
+                    datum: "-",
+                    uhrzeit: "-",
+                    probe: "-",
+                    leiter: "-",
+                    tag: "-"
+                })
+                throw err;
+            }
+            con.end();
 
             var stringDate = result[0].datum.toString();
             var dateFrom = dateFormat(stringDate, "dd.mm.yyyy");
             var day = wochentage(dateFormat(stringDate, "dddd"));
-            res.send({ datum: "02.04.2019", uhrzeit: "18:30", probe: "asdg", leiter: "125125", tag: "Montag" })
-            //res.send({ datum: dateFrom, uhrzeit: result[0].uhrzeit, probe: result[0].probe, leiter: result[0].leiter, tag: day })
-            res.send({ datum: "02.04.2019", uhrzeit: "18:30", probe: "asdg", leiter: "125125", tag: day })
+
+            res.send({
+                datum: dateFrom,
+                uhrzeit: result[0].uhrzeit,
+                probe: result[0].probe,
+                leiter: result[0].leiter,
+                tag: day
+            })
         });
+        /*
+         //if (err) {
+         //    res.send({ datum: "02.04.2019", uhrzeit: "18:30", probe: "asdg", leiter: "125125", tag: "Montag" })
+         //throw err;
+         //}
+         //con.end();
+
+         var stringDate = result[0].datum.toString();
+         var dateFrom = dateFormat(stringDate, "dd.mm.yyyy");
+         var day = wochentage(dateFormat(stringDate, "dddd"));
+         res.send({ datum: "02.04.2019", uhrzeit: "18:30", probe: "asdg", leiter: "125125", tag: "Montag" })
+         //res.send({ datum: dateFrom, uhrzeit: result[0].uhrzeit, probe: result[0].probe, leiter: result[0].leiter, tag: day })
+         res.send({ datum: "02.04.2019", uhrzeit: "18:30", probe: "asdg", leiter: "125125", tag: day })
+         }); */
     });
 
 
 });
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
     user(req.sessionStore.sessions, function (username) {
         var con = mysql.createConnection({
             host: "localhost",
