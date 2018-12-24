fe32e7bdcc503e43cea36d328ceff7e6f396ab04
hpfw <fwhp@web.de>
2018-12-24 14:53:02 +0100

    Update 5.0

--- Server/routes/aktuelles.js
+++ Server/routes/aktuelles.js
@@ -86,10 +86,10 @@ router.post('/', upload.array('aktuelles', 12), function (req, res) {
     });
 
     con.connect(function (err) {
-        if (err) throw err;
+       // if (err) throw err;
 
         con.query("SELECT * FROM einsaetze ORDER BY datum DESC", function (err, result) {
-            if (err) throw err;
+           // if (err) throw err;
             con.end();
             sortData(result, function (data) {
                 res.send({status: data});
