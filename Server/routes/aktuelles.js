a0b8e59d70a6c36d99d7d12a96e527accbfc17dc
hpfw <fwhp@web.de>
2018-12-24 12:14:17 +0100

    Update 5.0

--- Server/routes/aktuelles.js
+++ Server/routes/aktuelles.js
@@ -33,7 +33,7 @@ router.get('/', function (req, res) {
         if (err) throw err;
 
         con.query("SELECT * FROM aktuelles WHERE datum <= " + mysql.escape(date) + " ORDER BY datum DESC LIMIT 1", function (err, result, fields) {
-        //con.query("SELECT * FROM aktuelles LIMIT 1", function (err, result, fields) {
+            //con.query("SELECT * FROM aktuelles LIMIT 1", function (err, result, fields) {
             if (err) throw err;
             con.end();
             res.send({text: result[0].text, bild: result[0].bild})
@@ -52,34 +52,33 @@ router.post('/', upload.array('aktuelles', 12), function (req, res) {
     console.log(req.body);
     console.log(req.files);
 
-    res.send({status: req.files})
+    // res.send({status: req.files})
 
 
-    /* res.send({status: req.cookies})
-     user(req.sessionStore.sessions, function (username) {
-     var con = mysql.createConnection({
-     host: "localhost",
-     port: "3306",
-     user: "feuerweh",
-     password: "Feuerwehr!?123FFW!",
-     database: "feuerweh_"
-     });
-     var values = []
-     req.body.data.push(username)
-     values.push(req.body.data)
+    //  res.send({status: req.cookies})
+    //    user(req.sessionStore.sessions, function (username) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "feuerweh",
+        password: "Feuerwehr!?123FFW!",
+        database: "feuerweh_"
+    });
+    var values = []
+    values.push(req.body.data)
 
-     con.connect(function (err) {
-     if (err) throw err;
+    con.connect(function (err) {
+        if (err) throw err;
 
-     con.query("INSERT INTO aktuelles (text, bild, datum, username) VALUES ?", [values], function (err, result) {
-     if (err) throw err;
-     con.end();
+        con.query("INSERT INTO aktuelles (text, bild, datum ) VALUES ?", [values], function (err, result) {
+            if (err) throw err;
+            con.end();
 
-     res.send({status: req.sessionStore.sessions})
-     res.send({status: result})
-     });
-     });
-     })*/
+            //res.send({status: req.sessionStore.sessions})
+            res.send({status: result})
+        });
+
+    })
 });
 
 router.put('/', function (req, res) {
