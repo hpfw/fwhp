3aa2d8cb94489abfeee2dd57b0824fe0b1a6be8c
hpfw <fwhp@web.de>
2018-12-25 10:17:28 +0100

    Portal mit Upload etc funktioniert -> fertig!

--- React_db/src/js/reducers/index.js
+++ React_db/src/js/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux"
-import { loadingBarReducer } from 'react-redux-loading-bar'
 
 import termine_fw_red from "./termine_fw_red"
 import termine_jf_red from "./termine_jf_red"
@@ -13,6 +12,4 @@ export default combineReducers({
     aktuelles_red,
     kontakt_red,
     einsaetze_red,
-    aktuelles_image_red,
-    loadingBar: loadingBarReducer,
 })
