0e30aad65688d79902f2d8cc3acf585c09265b48
Gandalf <fwhp@web.de>
2019-01-12 16:11:32 +0100

    beschreibung hänger

--- React/src/js/pages/Fahrzeuge.js
+++ React/src/js/pages/Fahrzeuge.js
@@ -34,18 +34,31 @@ export default class Fahrzeuge extends React.Component {
             <div class="fahrzeuge">
 
                 <div>
-                    <div id="topFontContainer">LF10</div>
+                    <div id="topFontContainer">LF10 - Funkrufname: Waldburg 42</div>
                     <img src={"../../images/fahrzeuge/lf10/lf10ges.jpg"} width="100%" height="100%"/>
                 </div>
 
                 <div>
-                    <div id="fahrzeuge">LF16</div>
+                    <div id="fahrzeuge">LF16/12 - Funkrufname: Waldburg 44</div>
                     <img src={"../../images/fahrzeuge/lf16/lf16ges.jpg"} width="100%" height="100%"/>
                 </div>
 
                 <div>
-                    <div id="fahrzeuge">MTW</div>
+                    <div id="fahrzeuge">MTW - Funkrufname: Waldburg 19</div>
                     <img src={"../../images/fahrzeuge/mtw/mtwges.jpg"} width="100%" height="100%"/>
+                </div>
+
+                <div>
+                    <div id="fahrzeuge">Anhänger</div>
+                    <img src={"../../images/fahrzeuge/anhaenger/anhaengerges.jpg"} width="100%" height="100%"/>
+                    <div id="fahrzeuge">Fa. Voss:</div>
+                    <div id="fahrzeugeTextKlein">Baujahr 2008</div>
+                    <div id="fahrzeugeTextKlein">Zul. Gesamtgewicht: 2,5 Tonnen</div>
+                    <div id="fahrzeuge">Beladung:</div>
+                    <div id="fahrzeugeTextKlein">400 Meter B-Schläuche in Buchten</div>
+                    <div id="fahrzeugeTextKlein">Ölbinder</div>
+                    <div id="fahrzeugeTextKlein">Verkehrsabsicherungsmaterial</div>
+                    <div id="fahrzeugeTextKlein">Wassersauger</div>
                 </div>
 
             </div>
